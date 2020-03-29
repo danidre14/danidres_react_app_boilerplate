098aea0887cd9777d56c09bfa409649f1180eb6d
Carlonn Rivers <danidre14@gmail.com>
2020-03-29 00:59:41 -0400

    Version 1.0.1: More plugins and custom js files. Now supports implicit jsx imports

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function App() {
+    return (<>
+        <h1>Your React Application Boilerplate</h1>
+    </>);
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <div>Your React Application Boilerplate</div>,
-  document.getElementById("app")
-);
+ReactDOM.render(<App />, document.getElementById("app"));
